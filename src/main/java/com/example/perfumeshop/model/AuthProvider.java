e7a02f4a161ea3bd331f9d793d944029bd951e44
DuyTan <nhoxbybyby142133@gmail.com>
2021-12-21 15:36:55 +0700

    Added User entity class

---
+++ src/main/java/com/example/perfumeshop/model/AuthProvider.java
@@ -0,0 +1,5 @@
+package com.example.perfumeshop.model;
+
+public enum AuthProvider {
+    LOCAL, GOOGLE, FACEBOOK
+}
---
+++ src/main/java/com/example/perfumeshop/model/Role.java
@@ -0,0 +1,5 @@
+package com.example.perfumeshop.model;
+
+public enum Role {
+    USER, ADMIN
+}
