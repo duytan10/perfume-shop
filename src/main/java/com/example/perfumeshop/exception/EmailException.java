864390745d45e438982c051108b657c83e2a19d5
DuyTan <nhoxbybyby142133@gmail.com>
2021-12-27 19:21:41 +0700

    Added exceptions for Register

---
+++ src/main/java/com/example/perfumeshop/exception/EmailException.java
@@ -0,0 +1,13 @@
+package com.example.perfumeshop.exception;
+
+import lombok.Getter;
+
+@Getter
+public class EmailException extends RuntimeException {
+
+    private final String emailError;
+
+    public EmailException(String emailError) {
+        this.emailError = emailError;
+    }
+}
